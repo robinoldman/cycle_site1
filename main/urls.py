53f5fb2e7e22fdf45cc3fc2470d8d0d712b54b87
robinoldman <>
2023-05-03 11:31:15 +0000

    create navbar

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,7 @@
-from django import pathfrom . import views
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
-    path ('',views.home,name='home'),
+    path('', views.home, name='home'),
 ]
