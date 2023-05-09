179a497299ddc131ef1a8e18107fef70eccfe960
robinoldman <>
2023-05-09 12:36:24 +0000

    add login page functionality to login page

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-]
+    path('login/', views.user_login, name='login'),
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'main/home.html')
+
+def user_login(request):
+    return render(request,'registration/login.html')
